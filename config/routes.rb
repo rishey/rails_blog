f2c052198435b1c5dd2228471562bea4c5503e7f
richard bluestein <richard@bluestein.com>
2013-10-06 23:15:51 -0500

    comments added

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Blog::Application.routes.draw do
   # You can have the root of your site routed with "root"
    # root 'welcome#index'
 
-   resources :posts
-   root to: "welcome#index"
+   resources :posts do
+    resources :comments
+   end
+   # root to: "welcome#index"
 
    # post GET /posts/:id(.:format)  posts#show
 
